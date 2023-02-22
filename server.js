8af441ac946b51b65a12e664e54942d3fb0fc91b
Alekseenko Nikita <72857598+forchello@users.noreply.github.com>
2023-02-22 13:46:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,26 +9,22 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 server.delete('/:resource/:id', (req, res) => {
-    try {
-        const resource = req.params.resource;
-        const id = Number(req.params.id);
+    const resource = req.params.resource;
+    const id = Number(req.params.id);
 
-        const data = bridge.fs.readFileSync('/vercel/path0/db.json', 'utf8');
-        const parsedData = JSON.parse(data);
-        const index = parsedData[resource].findIndex((item) => item.id === id);
+    const data = bridge.fs.readFileSync('/vercel/path0/db.json', 'utf8');
+    const parsedData = JSON.parse(data);
+    const index = parsedData[resource].findIndex((item) => item.id === id);
 
-        if (index === -1) {
-            return res.status(404).json({ message: 'Element not found' });
-        }
-
-        const result = parsedData[resource].filter((item) => item.id !== id);
-        parsedData[resource] = result;
-
-        bridge.fs.writeFileSync('/vercel/path0/db.json', JSON.stringify(parsedData));
-        res.status(200).json({ message: 'success' });
-    } catch (error) {
-        return res.status(400).json({ error });
+    if (index === -1) {
+        return res.status(404).json({ message: 'Element not found' });
     }
+
+    const result = parsedData[resource].filter((item) => item.id !== id);
+    parsedData[resource] = result;
+
+    bridge.fs.writeFileSync('/vercel/path0/db.json', JSON.stringify(parsedData));
+    res.status(200).json({ message: 'success' });
 });
 
 server.use(router)
