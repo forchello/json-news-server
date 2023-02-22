b262b3dbcd2ed7fed7525b7f6d7c260eef1ca130
Alekseenko Nikita <72857598+forchello@users.noreply.github.com>
2023-02-22 13:37:40 +0200

    vercel node bridge

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const jsonServer = require('json-server');
+const { createBridge } = require('@vercel/node-bridge');
+
+const bridge = createBridge();
 
 const server = jsonServer.create();
 
-const router = jsonServer.router('./tmp/db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -12,16 +15,18 @@ server.delete('/:resource/:id', (req, res) => {
         const resource = req.params.resource;
         const id = Number(req.params.id);
 
-        const data = router.db.get(resource).value();
-        const index = data.findIndex((item) => item.id === id);
+        const data = bridge.fs.readFileSync('db.json', 'utf8');
+        const parsedData = JSON.parse(data);
+        const index = parsedData[resource].findIndex((item) => item.id === id);
 
         if (index === -1) {
             return res.status(404).json({ message: 'Element not found' });
         }
 
-        const result = data.filter((item) => item.id !== id);
-        router.db.set(resource, result).write();
+        const result = parsedData[resource].filter((item) => item.id !== id);
+        parsedData[resource] = result;
 
+        bridge.fs.writeFileSync('db.json', JSON.stringify(parsedData));
         res.status(200).json({ message: 'success' });
     } catch (error) {
         return res.status(400).json({ error });
