45bed59bc3757555be839c07c53d47c2e66bda25
Alekseenko Nikita <72857598+forchello@users.noreply.github.com>
2023-02-22 13:29:07 +0200

    catching errors

--- server.js
+++ server.js
@@ -2,28 +2,30 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./tmp/db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
 server.delete('/:resource/:id', (req, res) => {
-    const resource = req.params.resource;
-    const id = Number(req.params.id);
+    try {
+        const resource = req.params.resource;
+        const id = Number(req.params.id);
 
-    // Найти объект в массиве, у которого значение свойства 'id' соответствует идентификатору 'id'
-    const data = router.db.get(resource).value();
-    const index = data.findIndex((item) => item.id === id);
+        const data = router.db.get(resource).value();
+        const index = data.findIndex((item) => item.id === id);
 
-    if (index === -1) {
-        return res.status(404).json({ message: 'Element not found' });
+        if (index === -1) {
+            return res.status(404).json({ message: 'Element not found' });
+        }
+
+        const result = data.filter((item) => item.id !== id);
+        router.db.set(resource, result).write();
+
+        res.status(200).json({ message: 'success' });
+    } catch (error) {
+        return res.status(400).json({ error });
     }
-
-    // Удалить найденный объект из массива
-    const result = data.filter((item) => item.id !== id);
-    router.db.set(resource, result).write();
-
-    res.status(200).json({ message: 'success' });
 });
 
 server.use(router)
